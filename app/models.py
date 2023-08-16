816425feca2216c560d0ebfeb9a65cd82dc6e3b5
Jake Matthews <jake.m86@gmail.com>
2023-08-16 07:14:27 -1000

    Added Post Table

--- app/models.py
+++ app/models.py
@@ -1,5 +1,7 @@
 from app import db
 from datetime import datetime
+from werkzeug.security import generate_password_hash, check_password_hash
+from random import randint
 
 # DDL Statement
 # CREATE TABLE user(
@@ -14,3 +16,28 @@ class User(db.Model):
     username = db.Column(db.String(75), nullable=False, unique=True)
     password = db.Column(db.String, nullable=False)
     date_created = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.password = generate_password_hash(kwargs.get('password'))
+
+    def __repr__(self):
+        return f"<User {self.id}|{self.username}>"
+    
+    def check_password(self, password_guess):
+        return check_password_hash(self.password, password_guess)
+    
+def random_photo():
+    return f"https://picsum.photos/500?random={randint(1,100)}"
+    
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(50), nullable=False)
+    body = db.Column(db.String, nullable=False)
+    image_url = db.Column(db.String, nullable=False, default=random_photo)
+    date_created = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)    
+
+    def __repr__(self):
+        return f"<Post {self.id}|{self.title}>"
--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,7 @@
-from app import app
+from app import app, db
 from flask import render_template, redirect, url_for
-from app.forms import SignUpForm
+from app.forms import SignUpForm, PostForm
+from app.models import User
 
 # Add a route
 @app.route('/')
@@ -17,9 +18,33 @@ def signup():
         username = form.username.data
         email = form.email.data
         password = form.password.data
-        print(first_name, last_name, username, email, password)
+        # print(first_name, last_name, username, email, password)
 
-        #redirect back to the home page
+      
+            # Check User table to see if there are any users with username or email
+        check_user = db.session.execute(db.select(User).where( (User.username==username) | (User.email==email) )).scalar()
+        if check_user:
+            print('A user with that username/password already exists')
+            return redirect(url_for('signup'))
+        # Create a new instance of the User class with data from form
+        new_user = User(first_name=first_name, last_name=last_name, username=username, email=email, password=password)
+        # Add the new_user object to the database
+        db.session.add(new_user)
+        db.session.commit()
+          #redirect back to the home page
         return redirect(url_for('index'))
+    
 
     return render_template('signup.html', form=form)
+
+@app.route('/create', methods=["GET", "POST"])
+def create_post():
+    form = PostForm()
+    if form.validate_on_submit():
+        
+        # Get data from the form
+        title = form.title.data
+        body = form.body.data
+        image_url = form.image_url.data or None
+        print(title, body, image_url)
+    return render_template('create_post.html', form=form)
