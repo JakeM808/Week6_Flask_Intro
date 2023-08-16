826e8c5f79905cd01c49853f297a1f3fedbfb12d
Jake Matthews <jake.m86@gmail.com>
2023-08-16 10:28:33 -1000

    login/logout

--- app/models.py
+++ app/models.py
@@ -1,14 +1,15 @@
-from app import db
+from app import db, login
 from datetime import datetime
 from werkzeug.security import generate_password_hash, check_password_hash
 from random import randint
+from flask_login import UserMixin
 
 # DDL Statement
 # CREATE TABLE user(
 #   id SERIAL PRIMARY KEY,
 #   first_name VARCHAR(50) NOT NULL,
 # )
-class User(db.Model):
+class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     first_name = db.Column(db.String(50), nullable=False)
     last_name = db.Column(db.String(50), nullable=False)
@@ -16,6 +17,7 @@ class User(db.Model):
     username = db.Column(db.String(75), nullable=False, unique=True)
     password = db.Column(db.String, nullable=False)
     date_created = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    posts = db.relationship('Post', backref='author')
 
 
     def __init__(self, **kwargs):
@@ -27,6 +29,10 @@ class User(db.Model):
     
     def check_password(self, password_guess):
         return check_password_hash(self.password, password_guess)
+    
+@login.user_loader
+def load_user(user_id):
+    return db.session.get(User, user_id)    
     
 def random_photo():
     return f"https://picsum.photos/500?random={randint(1,100)}"
--- app/routes.py
+++ app/routes.py
@@ -1,12 +1,14 @@
 from app import app, db
 from flask import render_template, redirect, url_for
-from app.forms import SignUpForm, PostForm
-from app.models import User
+from app.forms import SignUpForm, PostForm, LoginForm
+from app.models import User, Post
+from flask_login import login_user, logout_user
 
 # Add a route
 @app.route('/')
 def index():
-    return render_template('index.html')
+    posts = db.session.execute(db.select(Post).order_by(Post.date_created.desc())).scalars().all()
+    return render_template('index.html', posts=posts)
 
 @app.route('/signup', methods=["GET", "POST"])
 def signup():
@@ -31,20 +33,51 @@ def signup():
         # Add the new_user object to the database
         db.session.add(new_user)
         db.session.commit()
-          #redirect back to the home page
+
+               # Log the user in
+        login_user(new_user)
+        # redirect back to the home page
         return redirect(url_for('index'))
     
-
     return render_template('signup.html', form=form)
 
 @app.route('/create', methods=["GET", "POST"])
 def create_post():
     form = PostForm()
     if form.validate_on_submit():
-        
         # Get data from the form
         title = form.title.data
         body = form.body.data
         image_url = form.image_url.data or None
-        print(title, body, image_url)
+        # print(title, body, image_url)
+        
+        # Create a new post instance
+        new_post = Post(title=title, body=body, image_url=image_url, user_id=1)
+        # Add that object to the database
+        db.session.add(new_post)
+        db.session.commit()
+       
+        return redirect(url_for('index'))
     return render_template('create_post.html', form=form)
+
+app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
+@app.route('/login', methods=["GET", "POST"])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        username = form.username.data
+        password = form.password.data
+        print(username, password)
+        # Query the User table for a user with that username
+        user = db.session.execute(db.select(User).where(User.username==username)).scalar()
+        # If we have a user AND the password is correct for that user
+        if user is not None and user.check_password(password):
+            # log the user in via login_user function
+            login_user(user)
+            return redirect(url_for('index'))
+
+    return render_template('login.html', form=form)
