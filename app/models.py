4ed26dfb47893ace2ac0c99514b78e93a4532efc
Jake Matthews <jake.m86@gmail.com>
2023-08-15 18:56:05 -1000

    add models page

---
+++ app/models.py
@@ -0,0 +1,16 @@
+from app import db
+from datetime import datetime
+
+# DDL Statement
+# CREATE TABLE user(
+#   id SERIAL PRIMARY KEY,
+#   first_name VARCHAR(50) NOT NULL,
+# )
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(50), nullable=False)
+    last_name = db.Column(db.String(50), nullable=False)
+    email = db.Column(db.String(75), nullable=False, unique=True)
+    username = db.Column(db.String(75), nullable=False, unique=True)
+    password = db.Column(db.String, nullable=False)
+    date_created = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
